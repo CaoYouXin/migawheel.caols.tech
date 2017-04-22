32ee7bed1fb8513db524ddb01ed031baba9f4933
曹力升 <c_youxin@163.com>
2017-04-22 16:39:35 +0800

    image list pass test.

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "@angular/core"
 import {Router} from "@angular/router";
+import {DomSanitizer, SafeStyle} from "@angular/platform-browser";
 
 @Component({
     selector: 'category',
@@ -14,10 +15,12 @@ export class CategoryComponent {
     private articleContent: string;
     private articleScriptSrc: string;
 
+    private imageListItemBackgroundSize: string;
     private imageList: ListItem[];
     private noneImageList: ListItem[];
 
-    constructor(private router: Router) {}
+    constructor(private router: Router,
+                private sanitizer: DomSanitizer) {}
 
     // ng handlers
     ngOnInit() {
@@ -29,10 +32,13 @@ export class CategoryComponent {
             '<button onclick="alert(\'hello world\')">hello world</button>';
         this.articleScriptSrc = 'http://caols.tech/a.js';
 
+        this.imageListItemBackgroundSize = 'contain';
         let item = new ListItem();
+        item.imageSrc = this.sanitizer.bypassSecurityTrustStyle('url("http://caols.tech/demos/1492848706508.png")');
         item.title = '标题';
         item.brief = '一个人的情绪受环境的影响，这是很正常的，但你苦着脸，一副苦大仇深的样子，对处境并不会有任何的改变，相反，如果微笑着去生活，那会增加亲和力，别人更乐于跟你交往，得到的机会也会更多。';
-        this.noneImageList = [item, JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item))];
+        this.imageList = [item, JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item))];
+        // this.noneImageList = [item, JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item))];
     }
 
     // dom handlers
@@ -50,5 +56,5 @@ export class CategoryComponent {
 export class ListItem {
     title: string;
     brief: string;
-    imageSrc: string;
+    imageSrc: SafeStyle;
 }
