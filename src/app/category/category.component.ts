1540ae33701fbf331919d705fdbae2d2afe3d00e
曹力升 <c_youxin@163.com>
2017-04-22 16:47:13 +0800

    generate to docs

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -37,8 +37,8 @@ export class CategoryComponent {
         item.imageSrc = this.sanitizer.bypassSecurityTrustStyle('url("http://caols.tech/demos/1492848706508.png")');
         item.title = '标题';
         item.brief = '一个人的情绪受环境的影响，这是很正常的，但你苦着脸，一副苦大仇深的样子，对处境并不会有任何的改变，相反，如果微笑着去生活，那会增加亲和力，别人更乐于跟你交往，得到的机会也会更多。';
-        this.imageList = [item, JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item))];
-        // this.noneImageList = [item, JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item))];
+        // this.imageList = [item, JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item))];
+        this.noneImageList = [item, JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item)), JSON.parse(JSON.stringify(item))];
     }
 
     // dom handlers
