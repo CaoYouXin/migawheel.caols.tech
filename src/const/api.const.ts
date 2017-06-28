efb884abfafe87003bcdf3b3bdcd7e11135468f8
曹力升 <c_youxin@163.com>
2017-06-28 12:55:51 +0800

    api prod

--- src/const/api.const.ts
+++ src/const/api.const.ts
@@ -6,128 +6,128 @@ export class API {
 
     static api = {
         "categories": {
-            "prod": "/blog_api/category/list",
-            "dev": "http://localhost:8080/blog_api/category/list"
+            "prod": `http://server.caols.tech:9003/category/list`,
+            "dev": `http://${document.domain}:8080/blog_api/category/list`
         },
         "category": {
             "prod": function (name) {
-                return `/blog_api/category/fetch_by_name?name=${name}`;
+                return `http://server.caols.tech:9003/category/fetch_by_name?name=${name}`;
             },
             "dev": function (name) {
-                return `http://localhost:8080/blog_api/category/fetch_by_name?name=${name}`;
+                return `http://${document.domain}:8080/blog_api/category/fetch_by_name?name=${name}`;
             }
         },
         "posts": {
             "prod": function (category) {
-                return `/blog_api/post/list_by_category?platform=All,Pc,Mobile&category=${category}`;
+                return `http://server.caols.tech:9003/post/list_by_category?platform=All,Pc,Mobile&category=${category}`;
             },
             "dev": function (category) {
-                return `http://localhost:8080/blog_api/post/list_by_category?platform=All,Pc,Mobile&category=${category}`;
+                return `http://${document.domain}:8080/blog_api/post/list_by_category?platform=All,Pc,Mobile&category=${category}`;
             }
         },
         "post": {
             "prod": function (name) {
-                return `/blog_api/post/fetch_by_name?name=${name}`;
+                return `http://server.caols.tech:9003/post/fetch_by_name?name=${name}`;
             },
             "dev": function (name) {
-                return `http://localhost:8080/blog_api/post/fetch_by_name?name=${name}`;
+                return `http://${document.domain}:8080/blog_api/post/fetch_by_name?name=${name}`;
             }
         },
         "like": {
             "prod": function (postId) {
-                return `/blog_api/feedback/like?postId=${postId}`;
+                return `http://server.caols.tech:9003/feedback/like?postId=${postId}`;
             },
             "dev": function (postId) {
-                return `http://localhost:8080/blog_api/feedback/like?postId=${postId}`;
+                return `http://${document.domain}:8080/blog_api/feedback/like?postId=${postId}`;
             }
         },
         "FetchComments": {
             "prod": function (postId) {
-                return `/blog_api/feedback/comment/list?postId=${postId}`;
+                return `http://server.caols.tech:9003/feedback/comment/list?postId=${postId}`;
             },
             "dev": function (postId) {
-                return `http://localhost:8080/blog_api/feedback/comment/list?postId=${postId}`;
+                return `http://${document.domain}:8080/blog_api/feedback/comment/list?postId=${postId}`;
             }
         },
         "CommentPost": {
-            "prod": "/blog_api/feedback/comment/post",
-            "dev": "http://localhost:8080/blog_api/feedback/comment/post"
+            "prod": `http://server.caols.tech:9003/feedback/comment/post`,
+            "dev": `http://${document.domain}:8080/blog_api/feedback/comment/post`
         },
         "CommentComment": {
             "prod": function(postId) {
-                return `/blog_api/feedback/comment/comment?postId=${postId}`;
+                return `http://server.caols.tech:9003/feedback/comment/comment?postId=${postId}`;
             },
             "dev": function (postId) {
-                return `http://localhost:8080/blog_api/feedback/comment/comment?postId=${postId}`;
+                return `http://${document.domain}:8080/blog_api/feedback/comment/comment?postId=${postId}`;
             }
         },
         "login": {
-            "prod": "/user_api/user/login",
-            "dev": "http://localhost:8080/user_api/user/login"
+            "prod": `http://server.caols.tech:9001/user/login`,
+            "dev": `http://${document.domain}:8080/user_api/user/login`
         },
         "register": {
-            "prod": "/user_api/user/register",
-            "dev": "http://localhost:8080/user_api/user/register"
+            "prod": `http://server.caols.tech:9001/user/register`,
+            "dev": `http://${document.domain}:8080/user_api/user/register`
         },
         "CheckUserName": {
             "prod": function (username) {
-                return `/user_api/username/check?username=${username}`;
+                return `http://server.caols.tech:9001/username/check?username=${username}`;
             },
             "dev": function (username) {
-                return `http://localhost:8080/user_api/username/check?username=${username}`;
+                return `http://${document.domain}:8080/user_api/username/check?username=${username}`;
             }
         },
         "captcha": {
             "prod": function (phone) {
-                return `/user_api/captcha?phone=${phone}`;
+                return `http://server.caols.tech:9001/captcha?phone=${phone}`;
             },
             "dev": function (phone) {
-                return `http://localhost:8080/user_api/captcha?phone=${phone}`;
+                return `http://${document.domain}:8080/user_api/captcha?phone=${phone}`;
             }
         },
         "FindPassword": {
             "prod": function (phone) {
-                return `/user_api/password/find?phone=${phone}`;
+                return `http://server.caols.tech:9001/password/find?phone=${phone}`;
             },
             "dev": function (phone) {
-                return `http://localhost:8080/user_api/password/find?phone=${phone}`;
+                return `http://${document.domain}:8080/user_api/password/find?phone=${phone}`;
             }
         },
         "ResetPassword": {
-            "prod": "/user_api/password/reset",
-            "dev": "http://localhost:8080/user_api/password/reset"
+            "prod": `http://server.caols.tech:9001/password/reset`,
+            "dev": `http://${document.domain}:8080/user_api/password/reset`
         },
         "CaptchaImage": {
             "prod": function (token) {
                 return `url(/user_api/captcha/image?token=${token})`;
             },
             "dev": function (token) {
-                return `url(http://localhost:8080/user_api/captcha/image?token=${token})`;
+                return `url(http://${document.domain}:8080/user_api/captcha/image?token=${token})`;
             }
         },
         "PreviousPost": {
-            "prod": "/blog_api/post/previous",
-            "dev": "http://localhost:8080/blog_api/post/previous"
+            "prod": `http://server.caols.tech:9003/post/previous`,
+            "dev": `http://${document.domain}:8080/blog_api/post/previous`
         },
         "NextPost": {
-            "prod": "/blog_api/post/next",
-            "dev": "http://localhost:8080/blog_api/post/next"
+            "prod": `http://server.caols.tech:9003/post/next`,
+            "dev": `http://${document.domain}:8080/blog_api/post/next`
         },
         "Top5Posts": {
-            "prod": "/blog_api/post/list_top_5",
-            "dev": "http://localhost:8080/blog_api/post/list_top_5"
+            "prod": `http://server.caols.tech:9003/post/list_top_5`,
+            "dev": `http://${document.domain}:8080/blog_api/post/list_top_5`
         },
         "SearchCategory": {
-            "prod": "/blog_api/search/category",
-            "dev": "http://localhost:8080/blog_api/search/category"
+            "prod": `http://server.caols.tech:9003/search/category`,
+            "dev": `http://${document.domain}:8080/blog_api/search/category`
         },
         "SearchPost": {
-            "prod": "/blog_api/search/post",
-            "dev": "http://localhost:8080/blog_api/search/post"
+            "prod": `http://server.caols.tech:9003/search/post`,
+            "dev": `http://${document.domain}:8080/blog_api/search/post`
         },
         "SearchPostWithCategory": {
-            "prod": "/blog_api/search/post_with_category",
-            "dev": "http://localhost:8080/blog_api/search/post_with_category"
+            "prod": `http://server.caols.tech:9003/search/post_with_category`,
+            "dev": `http://${document.domain}:8080/blog_api/search/post_with_category`
         }
     };
 
