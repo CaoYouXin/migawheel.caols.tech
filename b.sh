04d7103822094f839a82acaa2dc1831451459f6e
曹力升 <c_youxin@163.com>
2017-08-11 12:25:11 +0800

    404

--- b.sh
+++ b.sh
@@ -5,7 +5,8 @@ cd webapp \
     && cd .. \
     && rm -rf ./docs/ \
     && git checkout -- docs/CNAME \
-    && cp -r ./webapp/dist/ ./docs/
+    && cp -r ./webapp/dist/ ./docs/ \
+    && cp ./docs/index.html ./docs/404.html
 #    && git add --all \
 #    && git commit -m "$1" \
 #    && git push origin master
