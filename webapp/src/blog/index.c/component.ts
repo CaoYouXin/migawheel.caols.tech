84ad54aebdc845e511d9b911b757f87ab936c091
曹又心 <c_youxin@163.com>
2017-09-02 10:09:01 +0800

    fix small bu on window

--- webapp/src/blog/index.c/component.ts
+++ webapp/src/blog/index.c/component.ts
@@ -302,7 +302,7 @@ export class BlogIndexComponent implements OnInit {
   }
 
   svgMouseMove(e) {
-    if (!this.svgCanMove) {
+    if (!this.svgCanMove || e.movementX + e.movementY <= 5) {
       return;
     }
     this.svgMoving = true;
@@ -370,7 +370,7 @@ export class BlogIndexComponent implements OnInit {
     }
 
     if (!elem.source) {
-      return null;
+      return;
     }
 
     this.loading = true;
